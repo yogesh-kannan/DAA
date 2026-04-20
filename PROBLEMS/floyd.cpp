5ac4e1be1253abfeb92eddd458dd1c604f08607a
yogesh kannan <yogeshkannan.nk@gmail.com>
2026-04-20 11:47:12 +0530

    Create floyd.cpp

---
+++ PROBLEMS/floyd.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+#define INF 99999
+#define MAX 10
+
+int dist[MAX][MAX];
+int n;
+
+void floydWarshall() {
+    for (int k = 0; k < n; k++)
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                if (dist[i][k] + dist[k][j] < dist[i][j])
+                    dist[i][j] = dist[i][k] + dist[k][j];
+}
+
+void printMatrix() {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (dist[i][j] == INF)
+                cout << "INF ";
+            else
+                cout << dist[i][j] << "   ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    cout << "Enter number of vertices: ";
+    cin >> n;
+
+    cout << "Enter adjacency matrix:\n";
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            cin >> dist[i][j];
+
+    floydWarshall();
+
+    cout << "\nShortest distance matrix:\n";
+    printMatrix();
+
+    return 0;
+}
